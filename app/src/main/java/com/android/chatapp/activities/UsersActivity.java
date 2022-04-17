fc6c882a2bd9b3a31d414d01de9a72fc61298fb1
MaiThanhPhuc <maithanhphuc7721@gmail.com>
2022-04-17 10:01:27 +0700

    FE part2

--- app/src/main/java/com/android/chatapp/activities/UsersActivity.java
+++ app/src/main/java/com/android/chatapp/activities/UsersActivity.java
@@ -3,14 +3,42 @@ package com.android.chatapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
 
 import com.android.chatapp.R;
+import com.android.chatapp.databinding.ActivityUsersBinding;
+
 
 public class UsersActivity extends AppCompatActivity {
-
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_users);
+        binding =ActivityUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
+        getUsers();
+    }
+
+    private void setListeners(){
+        binding.imageBack.setOnClickListener(e -> onBackPressed());
+    }
+
+    private  void getUsers(){
+
+    }
+
+    private void showErrorMessage(){
+        binding.textErrorMessage.setText(String.format("%s","No user available"));
+    }
+    private  void loading(Boolean isLoading){
+        if(isLoading){
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
     }
 }
